28a8d10dbb6fbaf28d844f9bd71dcb28334a0141
olavostauros <olavostauros@outlook.com>
2025-06-09 19:33:59 -0300

    01-project-setup.prompt.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://charlespersonal.fit',
+  output: 'static',
+  compressHTML: true,
   integrations: [],
 });
